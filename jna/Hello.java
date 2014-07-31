d370fdd7d1bf888aa78a347f0d1f203f0e75b1c9
batasrki <spejic@gmail.com>
2014-07-31 12:31:00 -0400

    Various tests; Java program now sends in the URL for the RTMP source and a URL for the RTMP sink

--- jna/Hello.java
+++ jna/Hello.java
@@ -1,21 +1,23 @@
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Pointer;
 
 public class Hello {
     public interface TestHello extends Library {
-	public void helloFromC();
-	public void showGstVersion();
-	public void parameterIn(String param);
-	public String parameterOut(String param);
-	public void videoTestSource();
+	public Pointer showGstVersion();
+	public void videoTestSource(String source_url, String sink_url);
     }
 
     public static void main(String argv[]) {
 	TestHello ctest = (TestHello)Native.loadLibrary("hello", TestHello.class);
-	ctest.helloFromC();
-	ctest.showGstVersion();
-	ctest.parameterIn("This is a test");
-	System.out.println(ctest.parameterOut("This is a test"));
-	ctest.videoTestSource();
+	// for(int i = 0; i < 1000000; i++) {
+	//     Pointer addressOfSomething = ctest.showGstVersion();
+	//     String message = addressOfSomething.getString(0);
+	//     Native.free(Pointer.nativeValue(addressOfSomething));
+	//     System.out.println(i);
+	// }
+	String source = "rtmp://192.168.1.114:1935/yanked/stream-fancy";
+	String sink = "rtmp://192.168.1.114:1935/yanked/stream-videotestsrc";
+	ctest.videoTestSource(source, sink);
     }
 }
--- jna/hello.c
+++ jna/hello.c
@@ -1,103 +1,182 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <gst/gst.h>
 
-void helloFromC() {
-  printf("Hello from C.\n");
+typedef struct _CustomData {
+  GstElement *pipeline;
+  GstElement *source;
+  GstElement *sink;
+  gboolean is_live;
+  GMainLoop *loop;
+} CustomData;
+
+static void pad_added_handler(GstElement *source, GstPad *pad, CustomData *data) {
+  GstPad *sink_pad = gst_element_get_static_pad(data->sink, "sink");
+  GstPadLinkReturn retval;
+  GstCaps *new_pad_caps = NULL;
+  GstStructure *new_structure = NULL;
+  const gchar *new_pad_type = NULL;
+  g_print("Received new pad '%s' from '%s'\n", GST_PAD_NAME(pad), GST_ELEMENT_NAME(source));
+  new_pad_caps = gst_pad_query_caps(pad, NULL);
+  g_print("Getting the capabilities\n");
+  new_structure = gst_caps_get_structure(new_pad_caps, 0);
+  g_print("Getting the structures of capabilities\n");
+  new_pad_type = gst_structure_get_name(new_structure);
+  g_print("Getting the name: %s\n", new_pad_type);
+
+  g_print("g str has a prefix '%s': %d\n", new_pad_type, g_str_has_prefix(new_pad_type, "video/x-raw"));
+  if(g_str_has_prefix(new_pad_type, "video/x-raw")) {
+    g_print("Linking the video pad to the video converter sink\n");
+    g_print("Is pad linked: %d\n", gst_pad_is_linked(sink_pad));
+    if(gst_pad_is_linked(sink_pad)) {
+      g_print("Video pad linked already\n");
+      goto exit;
+    }
+    retval = gst_pad_link(pad, sink_pad);
+
+    if(GST_PAD_LINK_FAILED(retval)) {
+      g_print("Type is '%s', but linking failed\n", new_pad_type);
+    }
+    else {
+      g_print("Link succeeded with type '%s'.\n", new_pad_type);
+    }
+  }
+
+ exit:
+  if(new_pad_caps != NULL) {
+    gst_caps_unref(new_pad_caps);
+  }
+  gst_object_unref(sink_pad);
 }
 
-void videoTestSource() {
-  GstElement *test_source, *converter, *sink, *pipeline;
-  GstElement *encoder, *muxer;
+static void cb_message (GstBus *bus, GstMessage *msg, CustomData *data) {
+  switch (GST_MESSAGE_TYPE(msg)) {
+  case GST_MESSAGE_ERROR: {
+    GError *err;
+    gchar *debug;
+ 
+    gst_message_parse_error(msg, &err, &debug);
+    g_print ("Error: %s\n", err->message);
+    g_error_free (err);
+    g_free (debug);
+
+    gst_element_set_state(data->pipeline, GST_STATE_READY);
+    g_main_loop_quit (data->loop);
+    break;
+  }
+  case GST_MESSAGE_EOS:
+    /* end-of-stream */
+    gst_element_set_state (data->pipeline, GST_STATE_READY);
+    g_main_loop_quit (data->loop);
+    break;
+  case GST_MESSAGE_BUFFERING: {
+    gint percent = 0;
+
+    /* If the stream is live, we do not care about buffering. */
+    if (data->is_live) break;
+
+    gst_message_parse_buffering (msg, &percent);
+    g_print ("Buffering (%3d%%)\r", percent);
+    /* Wait until buffering is complete before start/resume playing */
+    if (percent < 100)
+      gst_element_set_state (data->pipeline, GST_STATE_PAUSED);
+    else
+      gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
+    break;
+  }
+  case GST_MESSAGE_CLOCK_LOST:
+    /* Get a new clock */
+    gst_element_set_state (data->pipeline, GST_STATE_PAUSED);
+    gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
+    break;
+  case GST_MESSAGE_STATE_CHANGED:
+    GST_DEBUG_BIN_TO_DOT_FILE(GST_BIN(data->pipeline), GST_DEBUG_GRAPH_SHOW_MEDIA_TYPE, "playing");
+    break;
+  default:
+    /* Unhandled message */
+    break;
+  }
+}
+
+void videoTestSource(const char* rtmp_source, const char* rtmp_sink) {
+  GstElement *test_source, *sink,*encoder, *muxer;
   GstBus *bus;
   GstMessage *message;
   GstStateChangeReturn return_value;
+  CustomData data;
+  GMainLoop *loop;
 
   gst_init(NULL, NULL);
-  test_source = gst_element_factory_make("videotestsrc", "source");
-  converter = gst_element_factory_make("videoconvert", "converter");
+  memset(&data, 0, sizeof(data));
+
+  test_source = gst_element_factory_make("rtmpsrc", "source");
   encoder = gst_element_factory_make("x264enc", "encoder");
   muxer = gst_element_factory_make("flvmux", "muxer");
   sink = gst_element_factory_make("rtmpsink", "sink");
-  pipeline = gst_pipeline_new("testsourcefromjavatortmp");
 
-  if(!pipeline || !test_source || !converter || !sink || !encoder || !muxer) {
+  data.source = gst_element_factory_make("decodebin", "decoder");
+  data.sink = gst_element_factory_make("videoconvert", "converter");
+  data.pipeline = gst_pipeline_new("testsourcefromjavatortmp");
+
+  g_object_set(encoder, "bframes", 0, NULL);
+  g_object_set(sink, "location", rtmp_sink, NULL);
+  g_object_set(test_source, "location", rtmp_source, NULL);
+
+  if(!data.pipeline || !test_source || !sink || !encoder || !muxer || !data.source || !data.sink) {
     g_printerr("Could create any elements\n");
   }
 
-  gst_bin_add_many(GST_BIN(pipeline), test_source, converter, encoder, muxer, sink, NULL);
+  gst_bin_add_many(GST_BIN(data.pipeline), test_source, encoder, muxer, sink, data.source, data.sink, NULL);
 
-  if(gst_element_link(test_source, converter) != TRUE) {
-    g_printerr("Could not connect the test source to the converter.\n");
-    gst_object_unref(pipeline);
+  if(gst_element_link(test_source, data.source) != TRUE) {
+    g_printerr("Could not connect the test source to the decoder.\n");
+    gst_object_unref(data.pipeline);
   }
 
-  if(gst_element_link(converter, encoder) != TRUE) {
+  if(gst_element_link(data.sink, encoder) != TRUE) {
     g_printerr("Could not connect the converter to the x264 encoder.\n");
-    gst_object_unref(pipeline);
+    gst_object_unref(data.pipeline);
   }
 
   if(gst_element_link(encoder, muxer) != TRUE) {
     g_printerr("Could not connect the encoder to the FLV muxer.\n");
-    gst_object_unref(pipeline);
+    gst_object_unref(data.pipeline);
   }
 
   if(gst_element_link(muxer, sink) != TRUE) {
     g_printerr("Could not link the converter to the ximage sink.\n");
-    gst_object_unref(pipeline);
+    gst_object_unref(data.pipeline);
   }
 
-  g_object_set(encoder, "bframes", 0, NULL);
-  g_object_set(muxer, "streamable", TRUE, NULL);
-  g_object_set(sink, "location", "rtmp://192.168.1.124:1935/yanked/stream-videotestsrc", NULL);
+  g_signal_connect(data.source, "pad-added", G_CALLBACK(pad_added_handler), &data);
+  bus = gst_element_get_bus(data.pipeline);
+  return_value = gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
 
-  return_value = gst_element_set_state(pipeline, GST_STATE_PLAYING);
   if(return_value == GST_STATE_CHANGE_FAILURE) {
     g_printerr("Unable to start playing the pipeline.\n");
-    gst_object_unref(pipeline);
+    gst_object_unref(data.pipeline);
+  }
+  else if(return_value == GST_STATE_CHANGE_NO_PREROLL) {
+    data.is_live = TRUE;
   }
 
-  bus = gst_element_get_bus(pipeline);
-  message = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+  loop = g_main_loop_new(NULL, FALSE);
+  data.loop = loop;
 
-  if(message != NULL) {
-    GError *error;
-    gchar *debug_info;
+  gst_bus_add_signal_watch(bus);
+  g_signal_connect(bus, "message", G_CALLBACK(cb_message), &data);
 
-    switch(GST_MESSAGE_TYPE(message)) {
-    case GST_MESSAGE_ERROR:
-      gst_message_parse_error(message, &error, &debug_info);
-      g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(message->src), error->message);
-      g_printerr("Debug info: %s\n", debug_info ? debug_info : "none");
-      g_clear_error(&error);
-      g_free(debug_info);
-      break;
-    case GST_MESSAGE_EOS:
-      g_print("End of stream reached.\n");
-      break;
-    default:
-      g_printerr("THIS SHOULD NOT HAPPEN\n");
-      break;
-    }
-    gst_message_unref(message);
-  }
-
+  g_main_loop_run(loop);
+  g_main_loop_unref(loop);
   gst_object_unref(bus);
-  gst_element_set_state(pipeline, GST_STATE_NULL);
-  gst_object_unref(pipeline);
+  gst_element_set_state(data.pipeline, GST_STATE_NULL);
+  gst_object_unref(data.pipeline);
 }
 
-/* void initPipeline() { */
-/*   pthread_create(&gst_app_thread, NULL, &videoTestSource, NULL); */
-/* } */
-
-/* void finalizePipeline() { */
-/*   pthread_join(gst_app_thread, NULL); */
-/* } */
-
-void showGstVersion() {
+const char* showGstVersion() {
   const gchar *nano_str;
   guint major, minor, micro, nano;
-  //  gst_init(&argc, &argv);
   gst_version(&major, &minor, &micro, &nano);
 
   if(nano == 1) {
@@ -110,7 +189,12 @@ void showGstVersion() {
     nano_str = "";
   }
 
-  printf("This program is linked against gstreamer version %d.%d.%d %s\n", major, minor, micro, nano_str);
+  char* message = (char *)malloc(100);
+
+  sprintf(message,\
+	  "This program is linked against gstreamer version %d.%d.%d %s\n", \
+	 major, minor, micro, nano_str);
+  return message;
 }
 
 void parameterIn(const char *param) {
